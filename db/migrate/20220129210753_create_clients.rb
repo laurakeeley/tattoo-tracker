cafb97d9fdeb23b6b15a980983f318af3b4574f9
laura <l.keeley000@gmail.com>
2022-01-29 15:08:52 -0600

    adds clients model

---
+++ db/migrate/20220129210753_create_clients.rb
@@ -0,0 +1,13 @@
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :profile_image
+      t.integer :tattoo_id
+
+      t.timestamps
+    end
+  end
+end
