51bdd6c0c58a1025598de5a42c254fa31d5cbe83
laura <l.keeley000@gmail.com>
2022-01-29 16:02:42 -0600

    adds happy/sad paths to create action, adds type to create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,18 @@ class UsersController < ApplicationController
   
 
   def create
-    client = Client.new(
+    user = User.new(
       name: params[:name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
-      profile_image: params[:profile_image]
+      profile_image: params[:profile_image],
+      type: params[:type]
     )
-    render json: client
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages} status: :bad_request
+    end
   end
 end
