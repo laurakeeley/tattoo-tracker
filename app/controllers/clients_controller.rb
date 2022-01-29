cc3b1c495a6eb5f5fcca226c3151b45cc7b834c6
laura <l.keeley000@gmail.com>
2022-01-29 15:36:29 -0600

    adds create action to routes file, create method in clients controller, and tested

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,14 @@
 class ClientsController < ApplicationController
+  
+
+  def create
+    client = Client.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      profile_image: params[:profile_image]
+    )
+    render json: client
+  end
 end
