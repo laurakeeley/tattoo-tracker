9b62ec12e7670f6edbce48fd9c080ab3baa65132
laura <l.keeley000@gmail.com>
2022-01-29 17:44:45 -0600

    adds tattoos to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create!([{name: "laura", email: "laura@gmail.com", password: "password", password_confirmation: "password", profile_image: "https://images.pexels.com/photos/1922528/pexels-photo-1922528.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"}])
-Client.create!([{name: "zia", email: "zia@gmail.com", password: "password", password_confirmation: "password", profile_image: "https://images.pexels.com/photos/4553022/pexels-photo-4553022.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"}])
-Client.create!([{name: "olga", email: "olga@gmail.com", password: "password", password_confirmation: "password", profile_image: "https://images.pexels.com/photos/6231881/pexels-photo-6231881.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"}])
+# User.create!([{name: "laura", email: "laura@gmail.com", password: "password", password_confirmation: "password", profile_image: "https://images.pexels.com/photos/1922528/pexels-photo-1922528.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"}])
+# Client.create!([{name: "zia", email: "zia@gmail.com", password: "password", password_confirmation: "password", profile_image: "https://images.pexels.com/photos/4553022/pexels-photo-4553022.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"}])
+# Client.create!([{name: "olga", email: "olga@gmail.com", password: "password", password_confirmation: "password", profile_image: "https://images.pexels.com/photos/6231881/pexels-photo-6231881.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"}])
+
+Tattoo.create!([{price: 430.00, time: "4:00"}, {price: 10.00, time: "00:30"}, {price: 50.00, time: "1:00"}])
