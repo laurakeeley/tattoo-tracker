cd4406c16c35b80eec3daeb7fb2162ace01b1c34
laura <l.keeley000@gmail.com>
2022-01-29 20:33:19 -0600

    corrects relationship create action in relationships controller, adds relationship create route to rest client

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,15 @@
 class RelationshipsController < ApplicationController
 
   def create
-    relationships = Relationships.new(
-      artist_id: params[:artist_id]
+    relationship = Relationship.new(
+      artist_id: params[:artist_id],
       client_id: params[:client_id]
     )
-    if relationships.save
-      render json: relationships
+
+    if relationship.save
+      render json: relationship
     else
-      render json: {errors: relationships.errors.full_messages}, status: :bad_request
+      render json: {errors: relationship.errors.full_messages}, status: :bad_request
     end
   end
 
